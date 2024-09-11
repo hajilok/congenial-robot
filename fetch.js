05c3c618309010d7e73689a3dc201e60b0b7fe90
Imam Wahyu Widodo <120608486+hajilok@users.noreply.github.com>
2024-09-11 16:50:17 +0700

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,19 @@
+fetch("https://api.poolam.xyz/balance/get", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "en-US,en;q=0.9",
+    "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE5MDUwOSwidGltZXN0YW1wIjoxNzI2MDQ3OTE4MjkyLCJ0eXBlIjoxLCJpYXQiOjE3MjYwNDc5MTgsImV4cCI6MTcyNjY1MjcxOH0.j1e5W8dI8L3WUrdinS8DQRKHqVMAl8LdQjfEzW7P33s",
+    "if-none-match": "W/\"b6-hYj0GONnD3qbVLuEjP34HavPvoo\"",
+    "priority": "u=1, i",
+    "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Microsoft Edge\";v=\"128\", \"Microsoft Edge WebView2\";v=\"128\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "Referer": "https://dapp.poolam.xyz/",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": null,
+  "method": "GET"
+});
